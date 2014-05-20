0e509509bbfca2d7fb3744820c4f7d73f3bb5fd9
Piedone <piedone@pyrocenter.hu>
2014-05-21 01:27:51 +0200

    Base Shape properties are accessible from templates now

--- Services/LiquidTemplateProcessor.cs
+++ Services/LiquidTemplateProcessor.cs
@@ -13,6 +13,8 @@ namespace Lombiq.LiquidMarkup.Services
     [OrchardFeature("Lombiq.LiquidMarkup.Templates")]
     public class LiquidTemplateProcessor : ITemplateProcessor
     {
+        private static bool _templateIsConfigured;
+
         private readonly ICacheService _cacheService;
 
 
@@ -30,19 +32,48 @@ namespace Lombiq.LiquidMarkup.Services
 
         public string Process(string template, string name, DisplayContext context = null, dynamic model = null)
         {
-            // Currently only global configuration is possible, see: https://github.com/formosatek/dotliquid/issues/93
-            Template.NamingConvention = new DotLiquid.NamingConventions.CSharpNamingConvention();
+            EnsureTemplateConfigured();
+
+            dynamic templateModel = null;
+
+            if (model is Orchard.DisplayManagement.Shapes.Shape)
+            {
+                templateModel = new Shape { Id = model.Id, Classes = model.Classes };
+            }
 
             var liquidTemplate = _cacheService.Get(template, () => Template.Parse(template));
             return liquidTemplate.Render(new RenderParameters
             {
+                LocalVariables = Hash.FromDictionary(new Dictionary<string, object>() { { "Model", templateModel } }),
                 RethrowErrors = true
             });
         }
 
         public void Verify(string template)
         {
+            EnsureTemplateConfigured();
+
             Template.Parse(template);
         }
+
+
+        // This method potentially runs from multiple threads, also the first time but this is safe to do.
+        private static void EnsureTemplateConfigured()
+        {
+            if (_templateIsConfigured) return;
+
+            // Currently only global configuration is possible, see: https://github.com/formosatek/dotliquid/issues/93
+            Template.NamingConvention = new DotLiquid.NamingConventions.CSharpNamingConvention();
+            Template.RegisterSafeType(typeof(Shape), new[] { "Id", "Classes" });
+
+            _templateIsConfigured = true;
+        }
+    }
+
+
+    public class Shape
+    {
+        public string Id { get; set; }
+        public IList<string> Classes { get; set; }
     }
 }
