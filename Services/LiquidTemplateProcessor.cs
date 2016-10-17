b3c07bc68eae05467c87188ff878d0f1229d8e3f
Albert Hajdu <albert.hajdu@lombiq.com>
2016-10-17 15:43:29 +0200

    Adding exception handling and displaying error message instead of YSOD
    --HG--
    branch : issue/NEST-200

--- Services/LiquidTemplateProcessor.cs
+++ Services/LiquidTemplateProcessor.cs
@@ -1,6 +1,12 @@
 ﻿using Orchard.DisplayManagement.Implementation;
 using Orchard.Environment.Extensions;
+using Orchard.Exceptions;
+using Orchard.Logging;
+using Orchard.Reports;
+using Orchard.Reports.Services;
 using Orchard.Templates.Services;
+using System;
+using System.Linq;
 
 namespace Lombiq.LiquidMarkup.Services
 {
@@ -11,6 +17,9 @@ namespace Lombiq.LiquidMarkup.Services
     public class LiquidTemplateProcessor : ITemplateProcessor
     {
         private readonly ILiquidTemplateService _templateService;
+        private readonly IReportsManager _reportsManager;
+
+        public ILogger Logger { get; set; }
 
 
         public string Type
@@ -19,15 +28,41 @@ namespace Lombiq.LiquidMarkup.Services
         }
 
 
-        public LiquidTemplateProcessor(ILiquidTemplateService templateService)
+        public LiquidTemplateProcessor(ILiquidTemplateService templateService, IReportsManager reportsManager)
         {
             _templateService = templateService;
+            _reportsManager = reportsManager;
+
+            Logger = NullLogger.Instance;
         }
-        
+
 
         public string Process(string template, string name, DisplayContext context = null, dynamic model = null)
         {
-            return _templateService.ExecuteTemplate(template, model);
+            try
+            {
+                return _templateService.ExecuteTemplate(template, model);
+            }
+            catch (Exception ex)
+            {
+                if (ex.IsFatal()) throw;
+
+                var errorMessage = "An unexpected exception was caught during rendering \"" + name + "\" Liquid template.";
+                Logger.Error(ex, errorMessage);
+
+                var reportActivityName = "LiquidTemplateErrors-" + name;
+
+                var liquidReport = _reportsManager
+                    .GetReports().FirstOrDefault(report => report.ActivityName == reportActivityName);
+
+                var liquidReportId = liquidReport == null
+                    ? _reportsManager.CreateReport("Liquid template errors - " + name, reportActivityName)
+                    : liquidReport.ReportId;
+
+                _reportsManager.Add(liquidReportId, ReportEntryType.Error, ex.Message);
+
+                return errorMessage + " " + ex.Message;
+            }
         }
 
         public void Verify(string template)
