f2ebc718d0c2216753138f747f21a7bd5263a110
Albert Hajdu <albert.hajdu@lombiq.com>
2016-10-18 16:20:46 +0200

    Making error message bold and red
    --HG--
    branch : issue/NEST-200

--- Services/LiquidTemplateProcessor.cs
+++ Services/LiquidTemplateProcessor.cs
@@ -66,7 +66,7 @@ namespace Lombiq.LiquidMarkup.Services
 
                 _reportsManager.Add(liquidReportId, ReportEntryType.Error, ex.Message);
 
-                return T("An unexpected exception was caught during rendering \"{0}\" Liquid template. {1}", name, ex.Message).Text;
+                return T("<b><font color=\"red\">An unexpected exception was caught during rendering \"{0}\" Liquid template. {1}</font></b>", name, ex.Message).Text;
             }
         }
 
